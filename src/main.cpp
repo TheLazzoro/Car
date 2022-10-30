95b72ce8042ec0de2e281b8b82c20a949d70d5d2
TheLazzoro <lasse.d@live.dk>
2022-10-30 16:45:50 +0100

    Basic wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,77 @@
+/*
 #include <Arduino.h>
-int motorPin = 3;
+byte myPin = D4;
+
+void setup()
+{
+  pinMode(myPin, OUTPUT);
+}
+
+void loop()
+{
+  delay(1000);
+  digitalWrite(myPin, HIGH);
+  delay(1000);
+  digitalWrite(myPin, LOW);
+}
+*/
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "WiFimodem-9846";
+const char* password = "jwmymdz4yw";
+
+
+WiFiServer server(80);
 
 void setup() {
+  Serial.begin(9600);
+  delay(10);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 0);
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  digitalWrite(5, 0);
+  digitalWrite(4, 0);
+  
+  digitalWrite(0, 1);
+  digitalWrite(2, 1);
 }
 
 void loop() {
-   pinMode(motorPin, OUTPUT);
-   analogWrite(motorPin, 50);
+  digitalWrite(5, HIGH);
+  digitalWrite(0, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(2, LOW);
+  delay(500);
+  digitalWrite(5, LOW);
+  digitalWrite(0, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(2, HIGH);
+  delay(10000);
 }
-
-/*
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
-  while (! Serial);
-  Serial.println("Speed 0 to 255");
-  
-} 
- 
- 
-void loop() 
-{ 
-  if (Serial.available())
-  {
-    int speed = Serial.parseInt();
-    if (speed >= 0 && speed <= 255)
-    {
-      Serial.println("speeed");
-      analogWrite(motorPin, speed);
-    }
-  }
-} 
-*/
