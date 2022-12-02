56a7b76832fb8efd1849d97c06334667edcd8bf3
TheLazzoro <lasse.d@live.dk>
2022-12-02 19:33:31 +0100

    Import ESPAsyncWebServer and AsyncTCP

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
 #include <string>
 
 /*
@@ -8,6 +9,14 @@
 	  B-           2             4 or D2
 	  B+           4             2 or D4
 */
+
+const char *ssid = "Car-Access-Point";
+const char *pass = "car";
+AsyncWebServer server(80);
+const char *PARAM_MESSAGE = "message";
+
+void action(AsyncWebServerRequest *request);
+void test(AsyncWebServerRequest *request);
 
 const int pwmMotorA = D1;
 const int pwmMotorB = D2;
@@ -23,46 +32,24 @@ bool isLeftReverse = false;
 
 std::string text;
 
-/// @brief Controls the car based on analogue inputs.
-/// @param x Turn signal 0 - 1023.
-/// @param y Move direction signal 0 - 1023.
-void controls(int x, int y)
-{
-	x = analogRead(D0) / 2 - 256;
-	y = analogRead(D1) / 2 - 256;
-	b = digitalRead(9);
-	Serial.print("\n");
-
-	float speedFactorA = y < 0 ? 1.0 : 1.0 - (float)y / (float)255;
-	float speedFactorB = y > 0 ? 1.0 : 1.0 + (float)y / (float)255;
-
-	motorSpeedA = x;
-	isRightReverse = motorSpeedA < 0;
-	if (isRightReverse)
-		motorSpeedA = x + (-2 * x);
-
-	motorSpeedA = motorSpeedA * speedFactorA;
-	if (motorSpeedA < 0)
-		motorSpeedA = 0;
-
-	motorSpeedB = x;
-	isLeftReverse = motorSpeedB < 0;
-	if (isLeftReverse)
-		motorSpeedB = x + (-2 * x);
-
-	motorSpeedB = motorSpeedB * speedFactorB;
-	if (motorSpeedB < 0)
-		motorSpeedB = 0;
-
-	Serial.print("B Speed: " + String(motorSpeedB) + "     A Speed: " + String(motorSpeedA) + "    revA: " + String(isRightReverse) + "   turn: " + String(y));
-
-	delay(10);
-}
-
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println();
+
+	Serial.println("Access point setup...");
+	WiFi.softAP(ssid);
+	// For some reason the network does not show up when setting a password.
+	// WiFi.softAP(ssid, pass);
+
+	IPAddress IP = WiFi.softAPIP();
+	Serial.print("IP: ");
+	Serial.println(IP);
+
+	server.on("/test", HTTP_GET, test);
+	server.on("/speed", HTTP_POST, action);
+	server.begin();
+	Serial.println("Server started.");
 
 	pinMode(pwmMotorA, OUTPUT);
 	pinMode(pwmMotorB, OUTPUT);
@@ -73,8 +60,31 @@ void setup()
 	delay(500);
 }
 
+void test(AsyncWebServerRequest *request)
+{
+	Serial.println("hiiiii"); // for debugging
+	request->send_P(200, "text/html", "response");
+}
+
+void action(AsyncWebServerRequest *request)
+{
+	String message;
+	if (request->hasParam(PARAM_MESSAGE, true))
+	{
+		message = request->getParam(PARAM_MESSAGE, true)->value();
+		Serial.println("action"); // for debugging
+	}
+	else
+	{
+		message = "No message sent";
+	}
+	request->send(200, "text/plain", "Hello, POST: " + message);
+}
+
 void loop()
 {
+	delay(10);
+	return;
 
 	Serial.println("Activate A");
 	analogWrite(pwmMotorA, motorSpeedA);
@@ -111,4 +121,40 @@ void loop()
 	digitalWrite(pwmMotorB, 0);
 	digitalWrite(dirMotorB, LOW);
 	delay(3000);
+}
+
+/// @brief Controls the car based on analogue inputs.
+/// @param x Turn signal 0 - 1023.
+/// @param y Move direction signal 0 - 1023.
+void controls(int x, int y)
+{
+	x = analogRead(D0) / 2 - 256;
+	y = analogRead(D1) / 2 - 256;
+	b = digitalRead(9);
+	Serial.print("\n");
+
+	float speedFactorA = y < 0 ? 1.0 : 1.0 - (float)y / (float)255;
+	float speedFactorB = y > 0 ? 1.0 : 1.0 + (float)y / (float)255;
+
+	motorSpeedA = x;
+	isRightReverse = motorSpeedA < 0;
+	if (isRightReverse)
+		motorSpeedA = x + (-2 * x);
+
+	motorSpeedA = motorSpeedA * speedFactorA;
+	if (motorSpeedA < 0)
+		motorSpeedA = 0;
+
+	motorSpeedB = x;
+	isLeftReverse = motorSpeedB < 0;
+	if (isLeftReverse)
+		motorSpeedB = x + (-2 * x);
+
+	motorSpeedB = motorSpeedB * speedFactorB;
+	if (motorSpeedB < 0)
+		motorSpeedB = 0;
+
+	Serial.print("B Speed: " + String(motorSpeedB) + "     A Speed: " + String(motorSpeedA) + "    revA: " + String(isRightReverse) + "   turn: " + String(y));
+
+	delay(10);
 }
